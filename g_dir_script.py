092f9f94e012ccb0102aee955de9c0a70e79ad30
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-11-18 10:47:15 -0800

    Find user by email address.

--- g_dir_script.py
+++ g_dir_script.py
@@ -1,7 +1,9 @@
 """This file mimics path where future scripts will be called from."""
+import os
 from tool_box import g_tools
 
-g_tools.find_user('jeremy.rodrigues@therealreal.com')
+my_work_email = os.environ["WORK_EMAIL"]
+g_tools.find_user(my_work_email)
 
 
 
