21b7db14b8ddfd26a41c2196af1c58c3e03ecb44
Jeremy Rodrigues <jeremy@jeremy-lime.local>
2019-12-09 16:19:29 -0800

    Try out print command.

--- g_dir_script.py
+++ g_dir_script.py
@@ -1,8 +1,11 @@
 """This file mimics path where future scripts will be called from."""
+import os
 from pprint import pprint
 
 from tool_box import g_tools
 
+me = os.environ["WORK_EMAIL"]
 
+pprint(g_tools.find_user(me))
 
 
