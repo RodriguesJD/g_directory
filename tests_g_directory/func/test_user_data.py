cf526822295a696efc775da23ae36bc9b929f8aa
Jeremy Rodrigues <jeremy@jeremy-lime.local>
2020-01-16 13:18:19 -0800

    Decide if user is FTE.

---
+++ tests_g_directory/func/test_user_data.py
@@ -0,0 +1,4 @@
+from g_tool_box import g_tools
+
+
+# TODO check that all users have customSchema
---
+++ tests_g_directory/unit/test_user_tools.py
@@ -0,0 +1,19 @@
+from g_tool_box import user_tools
+
+
+def test_is_fte():
+    fte_user = "dijana.antic@li.me"
+    fte_ou = "/Lime IT"
+
+    user_is_fte = user_tools.is_fte(fte_ou, fte_ou)
+    assert user_is_fte
+
+    not_fte_user = "Not.fte@li.me"
+    not_fte_ou = "/Lime Support"
+
+    user_is_fte = user_tools.is_fte(not_fte_ou, not_fte_user)
+    assert not user_is_fte
+
+    except_user = fte_user
+    user_is_fte_exception = user_tools.is_fte(not_fte_ou, except_user)
+    assert user_is_fte_exception
