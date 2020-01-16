91e0b2bc71c62734d54c137cb01a91031b9de2b4
Jeremy Rodrigues <jeremy@jeremy-lime.local>
2020-01-16 13:58:23 -0800

    Find user is no longer case sensitive.

--- g_dir_script.py
+++
@@ -1,8 +0,0 @@
-"""This file mimics path where future scripts will be called from."""
-import os
-from pprint import pprint
-import  csv
-from g_tool_box import g_tools
-from googleapiclient.errors import HttpError
-
-g_tools.find_user("John.usry@li.me")
