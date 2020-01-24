430921b6dd7a40e374f0887603bf3c016f296fd6
Jeremy Rodrigues <jeremy@jeremy-lime.local>
2020-01-24 10:27:43 -0800

    Google credentials.

---
+++ g_tool_box/google_credentials.py
@@ -0,0 +1,47 @@
+"""
+For applications that use the OAuth 2.0 protocol to call Google APIs, you can use an OAuth 2.0 client ID
+to generate an access token. The token contains a unique identifier.
+
+For instructions on how to setup OAuth 2.0 got to
+https://support.google.com/googleapi/answer/6158849
+"""
+
+import pickle
+import os.path
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from pprint import pprint
+
+
+def google_creds():
+    """
+       This function handles auth and service for google's access to admin sdk directory api.
+
+       :return service: Rest API service object
+       """
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/admin.directory.user',
+              'https://www.googleapis.com/auth/admin.directory.group',
+              'https://www.googleapis.com/auth/admin.directory.orgunit',
+              'https://www.googleapis.com/auth/drive.metadata.readonly']
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return creds
